57657661770897886fb1620ce60e29af1668622e
bekionel <bekionel@gmail.com>
2017-07-16 20:44:19 +0300

    Creating logging subsystem and utilizing log4j for logging. Adding javadocs in CachePolicyConfig

---
+++ src/main/java/com/bekionel/httpproxy/Logging.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bekionel.httpproxy;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Socrates
+ */
+public class Logging {
+    public static final Logger centralLogging = Logger.getLogger("Central Logging");
+   
+    
+    
+}
